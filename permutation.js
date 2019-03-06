8ccd1c2d0b6790890f787c80756e209506a81568
Cjewett_work <Craig9907@gmail.com>
2019-03-05 20:20:18 -0800

    update with practice question binary search tree

--- permutation.js
+++ permutation.js
@@ -2,3 +2,40 @@
 
 // permutation basic I
 
+
+
+
+
+const Bl = function(root, depth, depthHash) {
+    let cDepth = depth + 1
+    
+    if (root.children === null){
+        if (!depthHash[cDepth]){
+            depthHash[cDepth] = root
+        }
+    }
+    Bl(root.children, cDepth, depthHash)
+}
+
+const BlHelp = function(root) {
+    depth = 0
+    depthHash = {}
+    Bl(root, depth, depthHash)
+
+    for(let i = 0; i < depthHash.length; i++){
+        if (!depthHash[i + 1]){
+            return depthHash[i]
+        }
+    }
+}
+
+// overview
+
+// time complexity
+
+// ask if i can walkthrough?
+
+//
+
+
+
