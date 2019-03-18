dfc439d33c573215b4f14bd7aece3f9b1b0bcc6b
Cjewett_work <Craig9907@gmail.com>
2019-03-18 06:58:36 -0700

    update with basic bfs algo

---
+++ monday_practice.js
@@ -0,0 +1,24 @@
+// bfs
+
+const bfs = function (root) {
+    q = [root]
+    
+    while (q.length > 0){
+        let size = queue.length
+
+        for (let = 0; i < size; i++){
+
+            if(q[i].left){
+                q.push(q[i].left)
+            }
+
+            if (q[i].right){
+                q.push(q[i].right)
+            }
+
+        }
+
+    }
+
+}
+
