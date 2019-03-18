33ee8432f8a8cc353aea190d6f5b067e41bbee61
Cjewett_work <Craig9907@gmail.com>
2019-03-18 07:03:03 -0700

    Update with grammer improvements

--- monday_practice.js
+++ monday_practice.js
@@ -5,36 +5,26 @@ const bfs = function (root) {
 
     while (q.length > 0){
         let size = queue.length
-
-        for (let = 0; i < size; i++){
-
+        for (let i = 0; i < size; i++){
             if(q[i].left){
                 q.push(q[i].left)
             }
-
             if (q[i].right){
                 q.push(q[i].right)
             }
-
         }
-
     }
-
 }
 
 
 // dfs
 
 const dfs = function (root) {
-
     if (root.left){
         dfs(root.left)
     }
-
     if (root.right){
         dfs(root.right)
     }
-
     console.log("passed the recursive calls")
-
 }
