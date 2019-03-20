b44efaed0bce9ef16b02b0e95c41a5fb97454e6b
Cjewett_work <Craig9907@gmail.com>
2019-03-20 08:53:59 -0700

    add removeDup LL elements

--- tuesday_practice.js
+++ tuesday_practice.js
@@ -11,9 +11,36 @@ const delMidLL = function(root){
         mid = mid.next
         end = end.next.next
     }
-    
+
     // start the mid point deletion
     prev.next = mid.next
     mid.next = null
+}
 
+const removeDupLL = function(root){
+
+    let hasSeen = {}
+    hasSeen[root.val] = true
+
+    let cur = root
+    let testNode = root.next
+    let nextTestNode = null
+
+    if (root === null){
+        return
+    }
+
+    while (testNode !== null){
+        if (!hasSeen[testNode.val]){
+            hasSeen[testNode.val] = true
+            cur.next = testNode
+            cur = testNode 
+            testNode = testNode.next
+        }  else {
+            nextTestNode = testNode.next
+            testNode.next = null
+            testNode = nextTestNode
+        }
+        
+    }
 }
