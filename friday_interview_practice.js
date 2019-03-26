997d2aab688bc0f22ddc317853d7df992279eeaf
Cjewett_work <Craig9907@gmail.com>
2019-03-25 21:36:34 -0700

    Add description 2

--- friday_interview_practice.js
+++ friday_interview_practice.js
@@ -3,3 +3,5 @@
 // your in a room with a random number of keys.
 
 //  these keys give you access to other rooms (or previously accessed rooms)
+
+// develop a function that can determine if all the possible rooms can be access give the keys available in each room
