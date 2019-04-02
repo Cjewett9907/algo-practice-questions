bbb6b4faf8d6dd71639292579bafcfe475510834
Cjewett_work <Craig9907@gmail.com>
2019-04-01 21:10:15 -0700

    Add permutation practice

--- friday_schedule.js
+++ friday_schedule.js
@@ -17,3 +17,23 @@
 
 // apply to 25 jobs next week
 
+// permutation
+
+// function getAllPermutations(str) {
+//     let results = [];
+  
+//     if (str.length === 1) {
+//       results.push(str);
+//       return results;
+//     }
+  
+//     for (let i = 0; i < str.length; i++) {
+//       let firstChar = str[i];
+//       let charsLeft = str.substr(0, i) + str.substr(i + 1);
+//       let innerPermutations = getAllPermutations(charsLeft);
+//       for (let j = 0; j < innerPermutations.length; j++) {
+//         results.push(firstChar + innerPermutations[j]);
+//       }
+//     }
+//     return results;
+//   }
