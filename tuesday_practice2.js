2f9c4ff67128c76c15bd68503a7e017467467099
Cjewett_work <Craig9907@gmail.com>
2019-04-02 21:22:50 -0700

    add sketon for is BT identical

--- tuesday_practice2.js
+++ tuesday_practice2.js
@@ -1,3 +1,14 @@
 //
 
 // write a function that takes 2 root nodes and determines if they are identical Binary trees.
+
+
+const isIdentical = function(rootX, rootY){
+
+    if (rootX === null && rootY === null){
+        return true
+    }
+
+
+
+}
