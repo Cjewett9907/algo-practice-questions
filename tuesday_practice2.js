17fa060af41cdc02fd0f8c39a52b255c00bbce03
Cjewett_work <Craig9907@gmail.com>
2019-04-02 21:29:04 -0700

    Update is identical with solution

--- tuesday_practice2.js
+++ tuesday_practice2.js
@@ -9,6 +9,29 @@ const isIdentical = function(rootX, rootY){
         return true
     }
 
+    if (rootX.left === null && !rootY.left === null ){
+        return false
+    } else if (!rootX.left === null && rootY.left === null ){
+        return false
+    } else if  (rootX.left === null && rootY.left === null){
+        return 
+    }
 
+    if (rootX.left === rootY.left) {
+        isIdentical(rootX.left, rootY.left)
+    }
+
+    
+    if (rootX.right === null && !rootY.right === null ){
+        return false
+    } else if (!rootX.right === null && rootY.right === null ){
+        return false
+    } else if  (rootX.right === null && rootY.right === null){
+        return 
+    }
+
+    if (rootX.right === rootX.right){
+        isIdentical(rootX.right, rootY.right)
+    }
 
 }
